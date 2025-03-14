c43a504232e356626d35d99fa128efe4ffad867c
Freddy Lyle <OdincoGaming@gmail.com>
2025-03-13 21:05:21 -0700

    look and feel update
    started playing with ui and aesthetics

--- Assets/Scripts/Bubbles/SpawnPointBehaviour.cs
+++ Assets/Scripts/Bubbles/SpawnPointBehaviour.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SpawnPointBehaviour : MonoBehaviour
 {
     [SerializeField] private BubbleRequestEventChannelSO channel;
-    [SerializeField] private List<BaseBubbleSO> bubbles;
-    [SerializeField] private Vector2 intervalRange = new(1, 20);
     [SerializeField] private List<TimerTier> timerTiers = new List<TimerTier>();
+    private List<TimerTier> _tmrTrs = new();
 
     private void OnEnable()
     {
+        PopulateTiers();
         SpawnAtRandomIntervals(0);
-        DifficultyShift();
+        TierShift();
+    }
+
+    private void PopulateTiers()
+    {
+        _tmrTrs.Clear();
+        foreach(TimerTier t in timerTiers)
+        {
+            _tmrTrs.Add(t);
+        }
     }
 
     private void SpawnAtRandomIntervals(float previousInflateTime)
     {
-        float randomInterval = Random.Range(intervalRange.x + previousInflateTime, intervalRange.y);
+        float randomInterval = Random.Range(_tmrTrs[0].intervalRange.x + previousInflateTime, _tmrTrs[0].intervalRange.y);
         Timer timer = Timer.Register
         (
             duration: randomInterval,
@@ -28,48 +38,29 @@ public class SpawnPointBehaviour : MonoBehaviour
     }
     private void LoopSARI()
     {
-        int ind = Random.Range(0, bubbles.Count);
-        channel.RaiseEvent(new(transform, bubbles[ind]));
-        SpawnAtRandomIntervals(bubbles[ind].inflationTime);
+        int ind = Random.Range(0, _tmrTrs[0].bubbles.Count);
+        channel.RaiseEvent(new(transform, _tmrTrs[0].bubbles[ind]));
+        SpawnAtRandomIntervals(_tmrTrs[0].bubbles[ind].inflationTime);
     }
-    private void DifficultyShift()
+    private void TierShift()
     {
-        float startFloat = intervalRange.y;
-        float dur = timerTiers[0].timeUntilNextTier;
+        float startFloat = _tmrTrs[0].intervalRange.y;
+        float dur = _tmrTrs[0].timeUntilNextTier;
         Timer timer = Timer.Register
         (
             duration: dur,
-            onComplete: () => LoopDS(),
-            onUpdate: secondsElapsed =>
-            {
-                float progress = secondsElapsed / dur;
-                progress = progress * progress * (3f - 2f * progress);
-                intervalRange.y = Mathf.Lerp(startFloat, timerTiers[0].maxTimeBetweenSpawns, progress);
-                secondsElapsed += Time.deltaTime;
-            },
+            onComplete: () => LoopTS(),
             isLooped: false,
             useRealTime: true
         );
     }
 
-    private void LoopDS()
+    private void LoopTS()
     {
-        intervalRange.y = timerTiers[0].maxTimeBetweenSpawns;
-
-        
-        if (timerTiers[0].bubblesToAdd.Count > 0)
+        _tmrTrs.RemoveAt(0);
+        if (_tmrTrs.Count > 0)
         {
-            foreach(BaseBubbleSO b in timerTiers[0].bubblesToAdd)
-            {
-                bubbles.Add(b);
-            }
-        }
-
-
-        timerTiers.RemoveAt(0);
-        if (timerTiers.Count > 0)
-        {
-            DifficultyShift();
+            TierShift();
         }
     }
 }
@@ -77,7 +68,8 @@ public class SpawnPointBehaviour : MonoBehaviour
 [System.Serializable]
 public class TimerTier
 {
-    public float maxTimeBetweenSpawns = 20;
+    public Vector2 intervalRange = new(1, 20);
     public float timeUntilNextTier = 300f;
-    public List<BaseBubbleSO> bubblesToAdd;
+    public List<BaseBubbleSO> bubbles;
+    public UnityEvent onEnd;
 }
